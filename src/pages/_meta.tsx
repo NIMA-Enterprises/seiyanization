74a0564b8628a047e89af2fc53523d1181de4fbc
Marko Bozic <markobozic346@gmail.com>
2024-01-11 07:00:16 +0100

    Setup basic routes and pages

---
+++ src/pages/_meta.tsx
@@ -0,0 +1,26 @@
+import { Meta } from "nextra";
+
+const meta: Meta = {
+  index: {
+    display: "hidden",
+    theme: {
+      layout: "full",
+      sidebar: false,
+    },
+  },
+  documentation: {
+    title: "Documentation",
+    type: "page",
+    href: "/",
+  },
+  guides: {
+    title: "Guides",
+    type: "page",
+  },
+  faq: {
+    title: "FAQ",
+    type: "page",
+  },
+};
+
+export default meta;
---
+++ theme.config.tsx
@@ -0,0 +1,12 @@
+import { Header } from "@/components/Header";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  project: {
+    link: "https://github.com/shuding/nextra",
+  },
+  logo: <p>SEI Docs by NIMA</p>,
+  logoLink: false,
+};
+
+export default config;
