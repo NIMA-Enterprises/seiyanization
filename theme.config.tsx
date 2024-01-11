04f7b18c4d06f004e95d2b8ed1ca456256017d38
Marko Bozic <markobozic346@gmail.com>
2024-01-11 07:49:51 +0100

    Add Footer

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,4 +1,5 @@
-import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
+
 import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
@@ -7,6 +8,9 @@ const config: DocsThemeConfig = {
   },
   logo: <p>SEI Docs by NIMA</p>,
   logoLink: false,
+  footer: {
+    component: Footer,
+  },
 };
 
 export default config;
