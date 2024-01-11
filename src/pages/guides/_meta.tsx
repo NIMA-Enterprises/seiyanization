0406c3102e011e62dbe4cc748189727483cd4eda
Marko Bozic <markobozic346@gmail.com>
2024-01-11 17:22:01 +0100

    Add example guide

---
+++ src/pages/guides/_meta.tsx
@@ -0,0 +1,14 @@
+import { Meta } from "nextra";
+
+const meta: Meta = {
+  "*": {
+    theme: {
+      layout: "default",
+      sidebar: true,
+      breadcrumb: true,
+      toc: true,
+    },
+  },
+  "guide-1-example": "Guide 1 example",
+};
+export default meta;
