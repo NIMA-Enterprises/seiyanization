da1ff5cd8a6d6065f1cb0589f174c0a45f615db5
Marko Bozic <markobozic346@gmail.com>
2024-01-12 07:47:58 +0100

    Fix responsiveness for roadmap/changelog components

--- src/app/home/HomeRoadMap.tsx
+++ src/app/home/HomeRoadMap.tsx
@@ -23,17 +23,17 @@ const HomeRoadMap: React.FC<{ className?: string }> = ({ className }) => {
         </Link>
       </div>
       <div className="relative">
-        <div className="bg-orange-400 w-full   absolute left-0 top-3 h-2" />
+        <div className="bg-orange-400 md:w-full mt-6 md:mt-0 w-2 h-full absolute left-0 -top-3 md:h-2" />
         <div className="grid grid-cols-auto-fill-full gap-4 md:grid-cols-4">
           {homeChangelogs.map((road) => (
             <div
               className="flex flex-col gap-2 pl-6 relative md:pt-6 md:pl-0"
               key={road.id}
             >
-              <div className="h-4 w-4 rounded-xl border border-orange-200 -mt-4 -ml-1 bg-white absolute" />
+              <div className="h-4 w-4 rounded-xl border -ml-7  md:-ml-1 border-orange-200 mt-2 md:-mt-10 bg-white absolute" />
               <Link
                 href={road.href}
-                className="text-md font-semibold mt-2 link-text-hover"
+                className="text-md font-semibold mt-2 text-link-hover"
               >
                 {road.title}
               </Link>
