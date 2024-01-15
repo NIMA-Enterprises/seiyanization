603fa4f6c1a151e7648fb5ec6be6ce304a841cac
ffilipovicc98 <ffilipovicc98@gmail.com>
2024-01-15 10:52:03 +0100

    feature: Remove test commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import "../styles/main.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  console.log('test');
   
   return <Component {...pageProps} />;
 }
