e39a96d9bb4d3783c59cc59ea3f8f23b697e2615
madjarevicn <nikola.madjarevic@gmail.com>
2024-01-16 18:38:20 +0100

    fix: Change copy

--- src/app/home/HomeIntroSection.tsx
+++ src/app/home/HomeIntroSection.tsx
@@ -6,10 +6,10 @@ const HomeIntroSection: React.FC<{ className?: string }> = ({ className }) => {
     <div className={cn("mt-6 mb-12 py-20 header rounded-xl", className)}>
       <div className="text-sei-heading-dark px-10">
         <h1 className="text-3xl md:text-5xl font-bold mb-4">
-          {"Welcome, Let's Get Seiyanized!"}
+          {"Welcome to Seiyanization, platform with an incentive to get everyone from 0 to becoming a SuperSeiyan!"}
         </h1>
         <p className="opacity-80 md:text-xl text-sei-subheading-dark">
-          Find developer guides, API references, tutorials, and more.
+            Find guides, products, latest ecosystem research, tools, how to’s, and much more helpful things to help you get started!
         </p>
       </div>
     </div>
