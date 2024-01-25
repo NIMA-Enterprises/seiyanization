5f3b5c9abf8abf252c8762e0683caec95b658ce7
Marko Bozic <markobozic346@gmail.com>
2024-01-25 14:07:25 +0100

    Remove unused code

--- src/common/constants.tsx
+++ src/common/constants.tsx
@@ -4,19 +4,9 @@ import {
   HomePost,
   Supporter,
   Topic,
-  SeiBuilder,
   Changelog,
-  HomeVideo,
 } from "@/lib/types";
-import {
-  Boxes,
-  Cog,
-  FileQuestion,
-  ScrollText,
-  Settings,
-  Vault,
-  Wrench,
-} from "lucide-react";
+import { Boxes, FileQuestion, ScrollText, Settings, Vault } from "lucide-react";
 
 // GLOBAL
 export const routes = {
@@ -75,18 +65,6 @@ export const homeCards = [
       </svg>
     ),
     href: routes.ecosystem,
-    // subItems: [
-    //   {
-    //     title: "CosmWasm projects",
-    //     href: `${routes.ecosystem}?filter=cosmwasm`,
-    //     isExternal: false,
-    //   },
-    //   {
-    //     title: "EVM projects",
-    //     href: `${routes.ecosystem}?filter=evm`,
-    //     isExternal: false,
-    //   },
-    // ],
   },
   {
     title: "Guides",
@@ -110,44 +88,6 @@ export const homeCards = [
     ),
     href: routes.guides,
   },
-  // {
-  //   title: "Open sourced resources",
-  //   description:
-  //     "Explore Sei’s, infrastructure solutions, and development tooling.",
-  //   icon: <Layers className="size-8 text-sei-main-light dark:text-sei-main-dark" />,
-  //   href: externalLinks.resources,
-  // },
-  // {
-  //   title: "Sei foundation",
-  //   description: "Learn more about Sei foundation.",
-  //   icon: <Layers className="size-8 text-sei-main-light dark:text-sei-main-dark" />,
-  //   href: externalLinks.seiFoundation,
-  // subItems: [
-  //   {
-  //     title: "Twitter accounts",
-  //     href: "/",
-  //     isExternal: true,
-  //   },
-  //   {
-  //     title: "Resources",
-  //     href: externalLinks.resources,
-  //     isExternal: true,
-  //   },
-  //   {
-  //     title: "Official documentation",
-  //     href: externalLinks.seiDocs,
-  //     isExternal: true,
-  //   },
-  // ],
-  // },
-
-  // {
-  //   title: "Ecosystem KOLs",
-  //   description:
-  //     "Explore Sei’s, infrastructure solutions, and development tooling.",
-  //   icon: <Layers className="size-8 text-sei-main-light dark:text-sei-main-dark" />,
-  //   href: routes.ecosystemKOls,
-  // },
   {
     title: "Job Board",
     description: "Explore job opportunities in the Sei ecosystem projects.",
@@ -168,25 +108,6 @@ export const homeCards = [
     ),
     href: routes.jobs,
   },
-  // {
-  //   title: "FAQ",
-  //   description: "Got questions? Browse our FAQs for quick answers.",
-  //   icon: (
-  //     <svg
-  //       xmlns="http://www.w3.org/2000/svg"
-  //       viewBox="0 0 24 24"
-  //       fill="currentColor"
-  //       className="size-8 text-sei-main-light dark:text-sei-main-dark"
-  //     >
-  //       <path
-  //         fillRule="evenodd"
-  //         d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm11.378-3.917c-.89-.777-2.366-.777-3.255 0a.75.75 0 0 1-.988-1.129c1.454-1.272 3.776-1.272 5.23 0 1.513 1.324 1.513 3.518 0 4.842a3.75 3.75 0 0 1-.837.552c-.676.328-1.028.774-1.028 1.152v.75a.75.75 0 0 1-1.5 0v-.75c0-1.279 1.06-2.107 1.875-2.502.182-.088.351-.199.503-.331.83-.727.83-1.857 0-2.584ZM12 18a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z"
-  //         clipRule="evenodd"
-  //       />
-  //     </svg>
-  //   ),
-  //   href: routes.faq,
-  // },
 ];
 export const TOOL_TAGS = {
   GENERAL: "General",
